dee4c6e96d35519eaa08af3281dcdc9ab2b6d2b9
Victor Wolmeister <wolmeistervictor@gmail.com>
2024-10-14 20:47:25 -0300

    Fix self update and build for multiple dists

--- src/commands/self-update.cmd.ts
+++ src/commands/self-update.cmd.ts
@@ -1,4 +1,5 @@
 import { exists, rename, rm, writeFile } from 'node:fs/promises';
+import os from 'node:os';
 import { Command } from '@commander-js/extra-typings';
 import got from 'got';
 import ora from 'ora';
@@ -23,7 +24,33 @@ export class SelfUpdateCommand implements BaseCommand {
           }
 
           spinner.text = 'downloading latest version';
-          const downloadRes = await got(latestRelease.assets[0].browserDownloadUrl, {
+          // switch (platform) {
+          //   case 'darwin':
+          //     return path.join(homedir, 'Library/Application Support/wowa/wowa.json');
+          //   case 'linux':
+          //     return path.join(homedir, '.config/wowa/wowa.json');
+          //   case 'win32':
+          //     return path.join(homedir, 'AppData/Roaming/wowa/wowa.json');
+          //   default: {
+          //     throw new Error(`Unsupported operating system: ${platform}`);
+          //   }
+          // }
+          const platform = os.platform();
+          const asset = latestRelease.assets.find((a) => {
+            switch (platform) {
+              case 'win32':
+                return a.name.includes('win64');
+              case 'linux':
+                return a.name.includes('linux64');
+              default:
+                throw new Error(`Unsupported operating system: ${platform}`);
+            }
+          });
+          if (asset === undefined) {
+            spinner.fail('release asset not found');
+            process.exit(1);
+          }
+          const downloadRes = await got(asset.browserDownloadUrl, {
             responseType: 'buffer',
           });
           if (!downloadRes.ok) {
